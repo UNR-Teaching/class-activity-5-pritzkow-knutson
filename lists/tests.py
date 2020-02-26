36ecbf9471760e4a7b29b17e739de9e22b7c66ad
drednaut <jaredknutson@nevada.unr.edu>
2020-02-25 16:50:56 -0800

    Front page HTML no generated from a template

--- lists/tests.py
+++ lists/tests.py
@@ -18,11 +18,9 @@ class HomePageTest(TestCase):
 
     def test_home_page_returns_correct_html(self):
         request = HttpRequest()  
-        response = home_page(request)  
+        response = self.client.get('/')
         html = response.content.decode('utf8')  
-        expected_html = render_to_string('home.html')
-        self.assertEqual(html, expected_html)
-        #self.assertTemplateUsed(response, 'wrong.html')
-        #self.assertTrue(html.startswith('<html>'))  
-        #self.assertIn('<title>To-Do lists</title>', html)  
-        #self.assertTrue(html.strip().endswith('</html>'))  
+        self.assertTrue(html.startswith('<html>'))  
+        self.assertIn('<title>To-Do lists</title>', html)  
+        self.assertTrue(html.strip().endswith('</html>'))  
+        self.assertTemplateUsed(response, 'home.html')
